15293dd5c757336cd812fe8b582fa6bdd9d90485
Axbth <neymarjr7510bb@gmail.com>
2025-08-21 20:39:35 +0530

    Add files via upload

---
+++ important.cpp
@@ -0,0 +1,37 @@
+🔐 IMPORTANT FOR LOADER MAKING 🔐
+
+🌐 PANEL URL:
+👉 https://public.vippannel.site/connect
+
+🗝️ AUTH KEY:
+👉 @SRC_BGMI
+
+📦 LIB FILE (Loader APK):
+👉 libVIPLOADER.so
+
+📲 USAGE INSTRUCTION (Hinglish) — SRC BGMI LOADER 🔐
+
+1️⃣ Panel Link 🖇️ ko apne Src mein add karo taaki loader authenticate ho sake.  
+2️⃣ Key (@SRC_BGMI) ka use karo license ko verify karne ke liye.  
+
+📢 VIRTUAL METHOD:-
+
+3️⃣ Isse Name Se `libVIPLOADER.so` Lib Replace Kro.  
+4️⃣ Virtual Mai Bgmi Ko Add Krke Run Kro.
+
+🔐 PANEL:-
+👉 https://public.vippannel.site/connect  
+🔑 KEY: @SRC_BGMI  
+📦 LIB FILE: libVIPLOADER.so  
+
+
+📢 NOTE :- PANNEL WAHI LOG USE KRO JINKE PASS NAHI HAI PRIVATE PANNEL WAGERA.
+
+
+📢 JOIN FOR MORE:
+🔗 @PRIVATE_SRC_FILES  
+🔗 @PRIVATE_SRC_FILES
+
+📢 JOIN FOR MORE:
+🔗 @PRIVATE_SRC_FILES  
+🔗 @PRIVATE_SRC_FILES
